72d2f48374352d3ca7e8ea455c3d0b1ed1946dab
Bhavyai Gupta <zbhavyai@gmail.com>
2022-06-11 19:20:45 -0600

    redirecting not found to home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Movies from './Movies';
-import NotFound from './NotFound';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
         <Route path='/search' element={<Movies />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='*' element={<Navigate to='/' />}></Route>
       </Routes>
     </BrowserRouter>
   );
