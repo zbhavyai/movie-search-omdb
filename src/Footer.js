c6d7a4a09ae21ba6cb6a1d2e9f29527457f9264d
Bhavyai Gupta <zbhavyai@gmail.com>
2022-06-11 23:39:30 -0600

    updating footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='navbar-expand-lg fixed-bottom navbar-expand-lg bg-dark p-2'>
-      <div className='container py-2 text-center'>
-        <a className='text-white footer-links' href='https://www.omdbapi.com/' target='_blank' rel='noreferrer'>
-          Powered by OMDb API
-        </a>
+    <footer className='navbar-expand-lg fixed-bottom navbar-expand-lg bg-dark p-4'>
+      <div className='container'>
+        <div className='float-left'>
+          <a className='text-muted footer-links' href='https://github.com/zbhavyai/movie-search-omdb' target='_blank' rel='noreferrer'>
+            <i className='bi bi-github'></i> for source code
+          </a>
+        </div>
+        <div className='float-right'>
+          <a className='text-muted footer-links' href='https://www.omdbapi.com/' target='_blank' rel='noreferrer'>
+            Powered by <strong>OMDb API</strong>
+          </a>
+        </div>
       </div>
     </footer>
   );
