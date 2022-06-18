2f02cdff242ee700ee0ea7831d7565ecd9c534db
Bhavyai Gupta <zbhavyai@gmail.com>
2022-06-18 11:44:10 -0600

    fixing header font

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
       <div className='container py-2'>
-        <Link className='navbar-brand header-font' to='/'>
+        <Link className='navbar-brand header-font fw-bold' to='/'>
           <img src={process.env.PUBLIC_URL + 'logo192.png'} alt='' width='30' height='30' className='mx-2 header-icon' />
           OMDb Search
         </Link>
