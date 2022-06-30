fc74c2197c45177eb7efc4762da2ff9da55abd95
Bhavyai Gupta <zbhavyai@gmail.com>
2022-06-30 16:41:23 -0600

    fixing logo pic resolution

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
       <div className='container py-2'>
         <Link className='navbar-brand header-font fw-bold' to='/'>
-          <img src={process.env.PUBLIC_URL + 'logo192.png'} alt='' width='30' height='30' className='mx-2 header-icon' />
+          <img src={process.env.PUBLIC_URL + '/logo192.png'} alt='' width='30' height='30' className='mx-2 header-icon' />
           OMDb Search
         </Link>
       </div>
